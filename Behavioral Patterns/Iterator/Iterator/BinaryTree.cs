ec65c943e8e3647a4f78a05dc707c4bece0054e1
viniciusou <viniciusou@gmail.com>
2020-03-16 22:49:08 -0300

    Added Iterator Pattern

---
+++ Behavioral Patterns/Iterator/Iterator/BinaryTree.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Iterator
+{
+    public class BinaryTree<T>
+    {
+        private Node<T> root;
+
+        public BinaryTree(Node<T> root)
+        {
+            this.root = root;
+        }
+
+        public IEnumerable<Node<T>> InOrder
+        {
+            get
+            {
+                IEnumerable<Node<T>> Traverse(Node<T> current)
+                {
+                    if (current.Left != null)
+                    {
+                        foreach (var left in Traverse(current.Left))
+                            yield return left;
+                    }
+                    yield return current;
+
+
+                    if (current.Right != null)
+                    {
+                        foreach (var right in Traverse(current.Right))
+                            yield return right;
+                    }
+                }
+
+                foreach(var node in Traverse(root))
+                    yield return node;
+            }
+        }
+    }
+}
---
+++ Behavioral Patterns/Iterator/Iterator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Iterator
+{
+    /// <summary>
+    /// The iterator pattern is a design pattern in which an iterator is used to traverse 
+    /// a container and access the container's elements. The iterator pattern decouples 
+    /// algorithms from containers.
+    /// 
+    /// Iterator is an object (or, in .NET, a method) that facilitates the traversal of
+    /// a data structure.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //   1
+            //  / \
+            // 2   3
+
+            // in-order: 213
+
+            var root = new Node<int>(1, new Node<int>(2), new Node<int>(3));
+
+            var tree = new BinaryTree<int>(root);
+
+            Console.WriteLine(string.Join(",", tree.InOrder.Select(x => x.Value)));
+        }
+    }
+}
