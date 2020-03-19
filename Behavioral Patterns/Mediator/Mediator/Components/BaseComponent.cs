e3a564921e2a5e0e80316c45a64565e1bf957912
viniciusou <viniciusou@gmail.com>
2020-03-19 20:12:19 -0300

    Added Mediator Pattern

---
+++ Behavioral Patterns/Mediator/Mediator/Components/BaseComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mediator.Components
+{
+    // The Base Component provides the basic functionality of storing a
+    // mediator's instance inside component objects.
+    public class BaseComponent
+    {
+        protected IMediator _mediator;
+
+        public BaseComponent(IMediator mediator = null)
+        {
+            _mediator = mediator;
+        }
+
+        public void SetMediator(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
---
+++ Behavioral Patterns/Mediator/Mediator/Program.cs
@@ -0,0 +1,35 @@
+﻿using Mediator.Components;
+using System;
+
+namespace Mediator
+{
+    /// <summary>
+    /// The mediator pattern defines an object that encapsulates how a set of objects interact.
+    /// With the mediator pattern, communication between objects is encapsulated within a 
+    /// mediator object. Objects no longer communicate directly with each other, but instead 
+    /// communicate through the mediator. This reduces the dependencies between communicating 
+    /// objects, thereby reducing coupling.
+    /// 
+    /// In this case the Mediator interface declares a method used by components to notify the
+    //  mediator about various events. The Mediator may react to these events and pass the 
+    //  execution to other components.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Component1 component1 = new Component1();
+            Component2 component2 = new Component2();
+
+            new ConcreteMediator(component1, component2);
+
+            Console.WriteLine("Client triggers operation A.");
+            component1.DoA();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client triggers operation D.");
+            component2.DoD();
+        }
+    }
+}
