935501541e30c43ebf54140858d28db94d0a8657
viniciusou <viniciusou@gmail.com>
2020-03-19 22:43:02 -0300

    Added Memento Pattern

---
+++ Behavioral Patterns/Memento/Memento/BankAccount.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace Memento
+{
+    public class BankAccount
+    {
+        private int balance;
+        private List<Memento> changes = new List<Memento>();
+        private int current;
+
+        public BankAccount(int balance)
+        {
+            this.balance = balance;
+            changes.Add(new Memento(balance));
+        }
+
+        public Memento Deposit(int amount)
+        {
+            balance += amount;
+            var memento = new Memento(balance);
+            changes.Add(memento);
+            ++current;
+            return memento;
+        }
+
+        public Memento Restore(Memento memento)
+        {
+            if (memento != null)
+            {
+                balance = memento.Balance;
+                changes.Add(memento);
+
+                return memento;
+            }
+
+            return null;
+        }
+
+        public Memento Undo()
+        {
+            if (current > 0)
+            {
+                var memento = changes[--current];
+                balance = memento.Balance;
+                return memento;
+            }
+            return null;
+        }
+
+        public Memento Redo()
+        {
+            if(current + 1 < changes.Count)
+            {
+                var memento = changes[++current];
+                balance = memento.Balance;
+                return memento;
+            }
+
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(balance)}: {balance}";
+        }
+    }
+}
---
+++ Behavioral Patterns/Memento/Memento/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Memento
+{
+    /// <summary>
+    /// The memento pattern is a software design pattern that provides the ability to 
+    /// restore an object to its previous state (undo via rollback).
+    /// 
+    /// 
+    /// Memento is a token/handle representing the system state. Lets us rollback to
+    /// the state when the token was generated. May or may not directly expose state
+    /// information.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bankAccount = new BankAccount(100);
+            var memento1 = bankAccount.Deposit(50); //150
+            var memento2 = bankAccount.Deposit(25); //175
+            Console.WriteLine(bankAccount);
+
+            bankAccount.Undo();
+            Console.WriteLine($"Undo 1: {bankAccount}");
+            bankAccount.Undo();
+            Console.WriteLine($"Undo 2: {bankAccount}");
+            bankAccount.Redo();
+            Console.WriteLine($"Redo: {bankAccount}");
+        }
+    }
+}
